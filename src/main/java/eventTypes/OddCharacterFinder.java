c1217fc3eb619f6a49d3153bae0ef33379e85712
Rob Haines <robhaines01@gmail.com>
2017-02-26 15:41:15 +0000

    Small Fixups

--- src/main/java/eventTypes/OddCharacterFinder.java
+++ src/main/java/eventTypes/OddCharacterFinder.java
@@ -18,12 +18,14 @@ public class OddCharacterFinder implements SimpleEventInterface {
 	private final String summary;
 	public OddCharacterFinder()
 	{
-		summary=new String("Non ASCII character found at position(s) <positions>");
+		summary=new String("Non ASCII character [<char>] found at position(s) <position>");
 	}
 	
 	@Override
 	public ArrayList<Event> checkLine(Line myLine, ArrayList<Line> buffer) {
 		ArrayList<Integer[]> positions=new ArrayList<Integer[]>();
+		
+		ArrayList<Event> eventList=new ArrayList<Event>();
 		
 		for (int i = 0; i < myLine.getRawData().length(); i++)
 		{
@@ -31,31 +33,20 @@ public class OddCharacterFinder implements SimpleEventInterface {
 		
 		    if ((ch < 32 && ch != 9) || ( ch > 127 && (ch !=0xFEFF)))
 		    {
-		    	positions.add(new Integer[] {i+1,(int)ch});
 		    	
+		    	Event event=new Event(myLine,this,Event.Priority.HIGH);
+		    	event.setLinePos(0);
+		    	HashMap<String,String> map=new HashMap<String,String>();
+		    	map.put("position",String.valueOf(i));
+		    	map.put("char", String.valueOf((int)ch));
+		    	event.setEventMetaData(map);
+		    	eventList.add(event);
 		    }
 		    //Process char
 		}
-		if (!positions.isEmpty())
-		{
-			StringBuffer eventText=new StringBuffer();
-			for (Integer[] index:positions)
-			{
-				eventText.append(" (char:"+index[0]+" offset:"+index[1]+")");
-			}
-				
-			
-			Event event=new Event(myLine,this,Event.Priority.HIGH);
-	    	event.setLinePos(0);
-	    	HashMap<String,String> map=new HashMap<String,String>();
-	    	map.put("positions",eventText.toString());
-	    	event.setEventMetaData(map);
-	    	
-			return new ArrayList<Event>(Arrays.asList(event));
+
 		
-		}
-		else
-			return null;
+		return eventList;
 		
 	}
 
