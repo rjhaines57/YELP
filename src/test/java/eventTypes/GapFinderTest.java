f95df9d37cbd0b37edfb05f3f550fdf6f4352c56
rhaines@kent.local.synopsys.com <rhaines@kent.local.synopsys.com>
2017-07-26 10:23:53 +0100

    Add new test

---
+++ src/test/java/eventTypes/GapFinderTest.java
@@ -0,0 +1,59 @@
+package eventTypes;
+
+import static org.junit.Assert.*;
+
+import org.junit.After;
+import org.junit.AfterClass;
+import org.junit.Before;
+import org.junit.BeforeClass;
+import org.junit.Test;
+
+public class GapFinderTest {
+
+	@BeforeClass
+	public static void setUpBeforeClass() throws Exception {
+	}
+
+	@AfterClass
+	public static void tearDownAfterClass() throws Exception {
+	}
+
+	@Before
+	public void setUp() throws Exception {
+	}
+
+	@After
+	public void tearDown() throws Exception {
+	}
+
+	@Test
+	public void testGapFinder() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testCheckLine() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetDescription() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetSummary() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testGetEventName() {
+		fail("Not yet implemented");
+	}
+
+	@Test
+	public void testProcessState() {
+		fail("Not yet implemented");
+	}
+
+}
