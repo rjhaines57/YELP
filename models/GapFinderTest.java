bdb758e1b84a63fe9f119ab66a473e52697e7801
rhaines@kent.local.synopsys.com <rhaines@kent.local.synopsys.com>
2017-07-26 18:38:32 +0100

    Remove model

--- models/GapFinderTest.java
+++ models/GapFinderTest.java
@@ -5,11 +5,12 @@ import com.coverity.primitives.Coverity;
 
 public class GapFinderTest {
 
+	/*
 
 	public void exitTestInBizarreFashion()
 	{
 		com.coverity.primitives.Coverity.killpath();
 	}
-
+*/
     
 }
