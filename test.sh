3eeb01ebd80cf0d26bbc73b8513ed9e73d4b869d
Rob Haines <rhaines@synopsys.com>
2022-06-29 09:15:41 +0100

    Add test

---
+++ test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+
+OWNER=${OWNER:-"Default Team"}
+MAIN_BRANCH=$(git symbolic-ref --short refs/remotes/origin/HEAD | awk -F/ '{print $NF}')
+CIRCLE_BRANCH=${CIRCLE_BRANCH:-"${MAIN_BRANCH}"}
+
+set
+
+if [[ "${CIRCLE_BRANCH}" = "${MAIN_BRANCH}" ]]; then
+  # run full scan on main branch
+  echo '## Running full scan on main branch ##'
+
+else
+
+  echo '## Running incremental scan on main branch ##'
